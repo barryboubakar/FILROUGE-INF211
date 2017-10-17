914710724ca242ac1754a4a8294cb01eece3f067
Yohann LE GALL <yohann.legall@telecom-bretagne.eu>
2017-10-17 15:11:55 +0200

    toto

--- CabinetRecrutement_EJB/ejbModule/eu/telecom_bretagne/cabinet_recrutement/service/IServiceOffreEmploi.java
+++ CabinetRecrutement_EJB/ejbModule/eu/telecom_bretagne/cabinet_recrutement/service/IServiceOffreEmploi.java
@@ -1,8 +1,26 @@
 package eu.telecom_bretagne.cabinet_recrutement.service;
 
+import java.util.List;
+
 import javax.ejb.Remote;
+import javax.persistence.Query;
+
+import eu.telecom_bretagne.cabinet_recrutement.data.model.OffreEmploi;
 
 @Remote
 public interface IServiceOffreEmploi {
 
+	  public OffreEmploi findById(Integer id);
+
+	  public List<OffreEmploi> findByEntreprise(int idEntreprise);
+
+	  public List<OffreEmploi> findBySecteurActiviteAndNiveauQualification(int idSecteurActivite, int idNiveauQualification);
+	  
+	  public List<OffreEmploi> findAll();
+	  
+	  public OffreEmploi persist(OffreEmploi offreEmploi);
+	  
+	  public OffreEmploi update(OffreEmploi offreEmploi);
+	  
+	  public void remove(OffreEmploi offreEmploi);
 }
--- CabinetRecrutement_EJB/ejbModule/eu/telecom_bretagne/cabinet_recrutement/service/ServiceSecteurActivite.java
+++ CabinetRecrutement_EJB/ejbModule/eu/telecom_bretagne/cabinet_recrutement/service/ServiceSecteurActivite.java
@@ -1,7 +1,14 @@
 package eu.telecom_bretagne.cabinet_recrutement.service;
 
+import java.util.List;
+
+import javax.ejb.EJB;
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
+
+import eu.telecom_bretagne.cabinet_recrutement.data.dao.EntrepriseDAO;
+import eu.telecom_bretagne.cabinet_recrutement.data.dao.SecteurActiviteDAO;
+import eu.telecom_bretagne.cabinet_recrutement.data.model.SecteurActivite;
 
 /**
  * Session Bean implementation class ServiceSecteurActivite
@@ -13,8 +20,30 @@ public class ServiceSecteurActivite implements IServiceSecteurActivite {
     /**
      * Default constructor. 
      */
+	@EJB private SecteurActiviteDAO secteurDAO;
+	
     public ServiceSecteurActivite() {
         // TODO Auto-generated constructor stub
     }
+
+	@Override
+	public SecteurActivite findById(Integer id) {
+		return secteurDAO.findById(id);
+	}
+
+	@Override
+	public List<SecteurActivite> findAll() {
+		return secteurDAO.findAll();
+	}
+
+	@Override
+	public SecteurActivite persist(SecteurActivite secteur) {
+		return secteurDAO.persist(secteur);
+	}
+
+	@Override
+	public SecteurActivite update(SecteurActivite secteur) {
+		return secteurDAO.update(secteur);
+	}
 
 }
